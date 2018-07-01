4ab99e0bad5a0f326589def1641207270af9fe63
Kaka <drkaka@Kakas-iMac.local>
2018-07-01 12:09:56 +0800

    Add exp, kid and user_id to JWT in sign method.
    Close #2

--- src/sign.js
+++ src/sign.js
@@ -1,5 +1,10 @@
 var jwt = require('jsonwebtoken');
 var cert = require('./pemfile').loadPemFileFromPathArg();
 
-var token = jwt.sign({foo: 'bar'}, cert, { algorithm: 'RS256'});
+var token = jwt.sign({user_id: 1}, cert, { 
+    algorithm: 'RS256',
+    expiresIn: 600, // 10 mins
+    keyid: "bafa4df017094783a2418477e6a963f7"
+});
+
 console.log(token);
